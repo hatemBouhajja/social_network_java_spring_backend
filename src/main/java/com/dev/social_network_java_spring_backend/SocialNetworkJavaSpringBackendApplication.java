043cedf7801a960c55ce4f537464765533ea6a79
hatemBouhajja <hatembouhajja@gmail.com>
2018-12-09 00:12:42 +0100

    Initial commit

---
+++ src/main/java/com/dev/social_network_java_spring_backend/SocialNetworkJavaSpringBackendApplication.java
@@ -0,0 +1,12 @@
+package com.dev.social_network_java_spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkJavaSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetworkJavaSpringBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/dev/social_network_java_spring_backend/SocialNetworkJavaSpringBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dev.social_network_java_spring_backend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SocialNetworkJavaSpringBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
